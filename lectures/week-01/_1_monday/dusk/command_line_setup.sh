23feaab054affb9da5f8833785baac8fdd15a2fe
Brett Levenson <bhlevenson@gmail.com>
2015-03-16 13:41:01 -0700

    Updated brew installation script so it installs more of the most important software

--- lectures/week-01/_1_monday/dusk/command_line_setup.sh
+++ lectures/week-01/_1_monday/dusk/command_line_setup.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 
-# This script must be run as the superuser to work correctly.
+# This script must be run as the superuser to work correctly. 
 # Also, you must change the permissions of this script to be executable, so before attempting to run it,
 # run: 'chmod +x ./command_line_setup.sh' before trying to run this script.
 # Be sure to run this script with the command 'sudo command_line_setup.sh'
 
-if [!-d /usr/local]; then
-    mkdir -p /usr/local;
+ROOTLEVEL=''
+HOMEDIR='~'
+
+if [ ! -d $ROOTLEVEL/usr/local ]; then
+	echo "Creating /usr/local dir"
+    mkdir -p $ROOTLEVEL/usr/local;
 fi
 
-# Check to make sure that a bash profile does not already exist, and if not, create the files
-if [!-f ~/.bash_profile] && [!-f ~/.profile]; then
-	cat <<EOT >> ~/.bash_profile
-	export PATH=/usr/local/sbin:/usr/local/bin:$PATH
-
-	if [ -f ~/.bash_aliases ]; then
-		. ~/.bash_aliases
-	fi
-EOT
-else
-	echo 'export PATH=/usr/local/sbin:/usr/local/bin:$PATH' >> ~/.bash_profile;
-	echo 'A .bash_profile or .profile already existed, so aliases have not been created.'
+if [ ! -d $HOMEDIR ]; then
+	echo "Creating home directory"
+	mkdir -p $HOMEDIR;
 fi
 
-# Now we create ~/.bash_aliases if it doesn't already exist
-if [!-f ~/.bash_aliases]; then
-	cat <<EOT >> ~/.bash_aliases
+# Now we create $HOMEDIR/.bash_aliases if it doesn't already exist
+if [ ! -f $HOMEDIR/.bash_aliases ]; then
+	touch $HOMEDIR/.bash_aliases
+	cat <<EOT >> $HOMEDIR/.bash_aliases
 		# ls aliases
 		alias ll='ls -l'
 		alias la='ls -A'
@@ -52,7 +48,7 @@ if [!-f ~/.bash_aliases]; then
 		alias gk='gitk --all&'
 
 		# Shortcut to Reload Bash Profile
-		alias reld='source ~/.bash_profile'
+		alias reld='source $HOMEDIR/.bash_profile'
 
 		# Aliases for Typos
 		alias gti='git'
@@ -61,25 +57,67 @@ if [!-f ~/.bash_aliases]; then
 EOT
 fi
 
-# Ok, now let's reload the terminal with the current settings
-if [ -f ~/.bash_profile ]; then
-	source ~/.bash_profile
+# Set up the correct path pattern
+PATH=$ROOTLEVEL/usr/local/sbin:$ROOTLEVEL/usr/local/bin:$PATH
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Now we run the homebrew installer
+# Update homebrew recipes
+brew update
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
 
-# Now we run brew doctor to make sure everything is working
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
 
-brew doctor
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+echo "Homebrew installation is complete."
+
+
+# Now create the bash profile with the correct path in there
+
+# Check to make sure that a bash profile does not already exist, and if not, create the files
+touch $HOMEDIR/.bash_profile
+cat <<EOT >> $HOMEDIR/.bash_profile
+	export PATH=$PATH:'$PATH'
+
+	if [ -f $HOMEDIR/.bash_aliases ]; then
+		. $HOMEDIR/.bash_aliases
+	fi
+EOT
+
+# Ok, now let's reload the terminal with the current settings
+if [ -f $HOMEDIR/.bash_profile ]; then
+	source $HOMEDIR/.bash_profile
+fi
+
+echo "Bash profile setup is complete and command line setup has been reloaded."
+
 
 # Now let's set the owner of the /usr/local directory and all subdirs so that we don't get install errors from npm etc.
-chown -R `whoami` /usr/local
+chown -R `whoami` $ROOTLEVEL/usr/local
+
+echo "Owner of /usr/local directory has been changed successfully."
+
+echo "Cleaning up homebrew installations"
+brew cleanup
 
 # Complete, notify user about next steps.
-echo "Installation is complete."
-echo "Command line setup is complete."
+echo "About homebrew:"
 echo "To install new software, you can now run 'brew install name_of_software'"
 echo "For example, to install the latest version of git, you'll run: 'brew install git'"
 echo "To upgrade software, you can run 'brew upgrade' or 'brew upgrade name_of_software'"
